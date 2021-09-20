68b66a8704d6a607845772e3de78cc6af830fcf3
rudreshsolanki97 <rudreshsolanki101@gmail.com>
2021-09-20 19:43:44 +0530

    rep. feed: use stable rpc instead of ws

--- src/helpers/feed.ts
+++ src/helpers/feed.ts
@@ -8,6 +8,7 @@ import { ReconnectableXdc3 } from '../classes/ReconnectableEvent';
 
 
 const XdcObject = new ReconnectableXdc3(NETWORK.ws);
+const xdc3_rpc = new Xdc3(new Xdc3.providers.HttpProvider(NETWORK.rpc));
 
 // setTimeout(() => {
 //   XdcObject.disconnect()
@@ -24,7 +25,7 @@ export const GeneralContractMethodView = (
 ): Promise<any> => {
   return new Promise(async (resolve, reject) => {
     try {
-      const xdc3 = XdcObject.get_xdc3;
+      const xdc3 = xdc3_rpc;
       const contract = new xdc3.eth.Contract(
         ABI as AbiItem[],
         REPUTATION_CONTRACT_ADDRESS
@@ -46,7 +47,7 @@ export const GeneralContractMethod = (
     try {
       console.log("method, params", method, params);
 
-      const xdc3 = XdcObject.get_xdc3;
+      const xdc3 = xdc3_rpc;
       const contract = new xdc3.eth.Contract(
         ABI as AbiItem[],
         REPUTATION_CONTRACT_ADDRESS
@@ -83,7 +84,7 @@ export const GeneralContractMethod = (
 export const GetAddressReputation = async (
   address: string
 ): Promise<number> => {
-  const xdc3 = XdcObject.get_xdc3;
+  const xdc3 = xdc3_rpc;
   const contract = new xdc3.eth.Contract(ABI as AbiItem[], REPUTATION_CONTRACT_ADDRESS);
   return await contract.methods.getReputation(address).call();
 };
@@ -92,7 +93,7 @@ export const UpdateAddresReputation = async (
   address: string,
   reputation: number
 ): Promise<boolean> => {
-  const xdc3 = XdcObject.get_xdc3;
+  const xdc3 = xdc3_rpc;
   const contract = new xdc3.eth.Contract(ABI as AbiItem[], REPUTATION_CONTRACT_ADDRESS);
   const currentReputation = await contract.methods.getReputation(address).call()
   global.logger.debug("reputation change for", address, "-> current:", currentReputation, "updated:", reputation, "are equal:", currentReputation == reputation);
