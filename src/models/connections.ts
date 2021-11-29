2fb63ee0e52d1b32feededab0e2c7fe07bc037d6
Debchakra Majumder <dmajumder4292@gmail.com>
2021-11-29 13:29:25 +0530

    Change Mongo Opts

--- src/models/connections.ts
+++ src/models/connections.ts
@@ -3,10 +3,16 @@ import { STORX_DB_URI, FEED_DB_URI } from '../config'
 
 export const connection_storx = mongoose.createConnection(STORX_DB_URI, {
   useNewUrlParser: true,
-  useUnifiedTopology: true,
+  useUnifiedTopology: false,
+  ssl:true,
+  replicaSet:"atlas-1im26p-shard-0",
+  authSource:"admin",
 });
 
 export const connection_feed = mongoose.createConnection(FEED_DB_URI, {
   useNewUrlParser: true,
-  useUnifiedTopology: true,
+  useUnifiedTopology: false,
+  ssl:true,
+  replicaSet:"atlas-1im26p-shard-0",
+  authSource:"admin",
 });
