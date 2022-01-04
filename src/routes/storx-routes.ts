d3ebcfa83ed8b94f10cb98a1e60f9cb9a619853d
BlocksScanIO <support@blocksscan.io>
2022-01-04 23:12:44 +0530

    disable add/remove staker routes.

--- src/routes/storx-routes.ts
+++ src/routes/storx-routes.ts
@@ -6,8 +6,8 @@ const router = express.Router();
 import * as StorxController from "../controllers/storx-controller"
 import { ValidateRequest } from '../middlewares/validateRequest';
 
-router.post("/add-staker", ValidateRequest([body('staker').isString().notEmpty(), body('reputation').isNumeric()]), StorxController.AddStaker);
-router.post("/remove-staker", ValidateRequest([body('staker').isString().notEmpty()]), StorxController.RemoveStaker);
+// router.post("/add-staker", ValidateRequest([body('staker').isString().notEmpty(), body('reputation').isNumeric()]), StorxController.AddStaker);
+// router.post("/remove-staker", ValidateRequest([body('staker').isString().notEmpty()]), StorxController.RemoveStaker);
 // router.post('/update-staker-reputation', ValidateRequest([body('staker').isString().notEmpty(), body('reputation').isNumeric()]), StorxController.UpdateReputation)
 
 export default router
